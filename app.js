9db300e12f90e5a35fc412a10bf27303dfb426eb
Vijaya Donthireddy <donmac@vijayas-mbp.lan>
2018-08-28 21:39:08 -0700

    calling another service in the same namespace in AKS

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 
 const express = require('express');
+const request = require('request');
 var app = express();
 
 
 app.get("/", (req, res) => {
     res.send("Hello AKS!!!");
+});
+
+app.get("/names", (req, res) => {
+    request("http://vjservice-service/got", function(error, response, body) {
+        console.log('body: ' + body);
+        res.send(body);
+    });
 });
 
 app.listen(80, '0.0.0.0');
